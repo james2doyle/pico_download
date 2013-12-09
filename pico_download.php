0642f25cc96d10a8bd31af532c2ef90df650624f
Philipp Schmitt <philipp@schmitt.co>
2013-12-09 19:50:14 +0100

    download_url and download_folder can now be configured in config.php Change default download directory New twig variable: {{ download_url }} Update README.md accordingly

--- pico_download.php
+++ pico_download.php
@@ -9,41 +9,50 @@
  */
 class Pico_Download {
 
-	private $download;
-	private $path;
-	// you can change this if you want to use a different folder
-	private $download_folder = 'content/';
+    private $download;
+    private $path;
+    // you can change this if you want to use a different folder
+    private $download_url = 'download';
+    private $download_folder = 'content/download/';
 
-	public function request_url(&$url)
-	{
-		if(strpos($url, 'download') !== false) {
-			$this->download = true;
-			// lets get the system path
-			$this->path = dirname(dirname(__DIR__)).'/'.str_replace('download/', $this->download_folder, $url);
-		} else {
-			$this->download = false;
-		}
-	}
+    public function config_loaded(&$settings) {
+        if (isset($settings['download_url']))
+            $this->download_url = $settings['download_url'];
+        if (isset($settings['download_folder']))
+            $this->download_folder = $settings['download_folder'];
+    }
 
-	public function before_render(&$twig_vars, &$twig) {
-		// are we on download? is there even a file there?
-		// if there is no file it will throw a 404
-		if ($this->download && file_exists($this->path)) {
-			$pathinfo = pathinfo($this->path);
-			// Override 404 header
-			header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-			// forget the custom mimetype, lets just download
-			header('Content-Type: application/x-download');
-			header('Content-Transfer-Encoding: Binary');
-			// we can use the pathinfo array to find the filename
-			header("Content-disposition: attachment; filename=\"$pathinfo[basename]\"");
-			ob_clean(); // clean the output buffer
-			flush();
-			echo readfile($this->path);
-			// Don't continue to render template
-			exit;
-		}
-	}
+    public function request_url(&$url)
+    {
+        if(strpos($url, $this->download_url) !== false) {
+            $this->download = true;
+            // lets get the system path
+            $this->path = dirname(dirname(__DIR__)).'/'.str_replace($this->download_url.'/', $this->download_folder, $url);
+        } else {
+            $this->download = false;
+        }
+    }
+
+    public function before_render(&$twig_vars, &$twig) {
+        // are we on download? is there even a file there?
+        // if there is no file it will throw a 404
+        if ($this->download && file_exists($this->path)) {
+            $pathinfo = pathinfo($this->path);
+            // Override 404 header
+            header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+            // forget the custom mimetype, lets just download
+            header('Content-Type: application/x-download');
+            header('Content-Transfer-Encoding: Binary');
+            // we can use the pathinfo array to find the filename
+            header("Content-disposition: attachment; filename=\"$pathinfo[basename]\"");
+            ob_clean(); // clean the output buffer
+            flush();
+            echo readfile($this->path);
+            // Don't continue to render template
+            exit;
+        }
+        $twig_vars['download_url'] = $this->download_url; // {{ download_url }}
+    }
 
 }
 
